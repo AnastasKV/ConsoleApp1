9a8d450be6001a443fbc33378be139bb0ba743db
Cab38-07 <Cab38-07@03-C38-07>
2021-06-18 12:29:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 0, 1, 2, 30, 4, 5, 6, 0, 7, 8, 0, 9, 10 };
+            Console.WriteLine($"Количество нулей - {array.Count(e => e == 0)}");
+        }
+    }
+}
